fd44e18637a3d14ec89935c98ad78f01f4499ee2
FathimaHusna <husnassf.19@uom.lk>
2024-09-23 17:51:00 +0530

    Product Categories Added

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from . models import Product
+from . models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
@@ -10,6 +10,18 @@ from django import forms
 
 # Create your views here.
 
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+    try:
+        category = Category.objects.get(name=foo)
+        products= Product.objects.filter(category = category)
+        return render(request, 'category.html', {'products': products, 'category': category})
+
+    except:
+        messages.success(request, ("That Category Doesn't Exist."))
+        return redirect('home')
+
+    
 def product(request, pk):
     product = Product.objects.get(id=pk)
     return render(request, 'product.html', {'product': product})
